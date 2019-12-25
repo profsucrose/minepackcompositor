72b575f507e9f9a4063921c59bfd07c23f3961c3
Professor Sucrose <dr.blobbles@gmail.com>
2019-12-25 12:31:29 -0800

    Updated init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ source .minepackcompositor/main.sh
 mv .minepackcompositor ~/.minepackcompositor
 echo 'source ~/.minepackcompositor/main.sh' >> ~/.zshrc
 echo 'source ~/.minepackcompositor/main.sh' >> ~/.bash_profile
+npm i
+rm init.sh
